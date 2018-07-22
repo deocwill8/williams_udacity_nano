d9f24b2b6f732934e6d885cb4ad2ad6618ff2608
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-07-22 18:06:43 -0500

    adding Player class to project

--- williams-classic-arcade-game/js/app.js
+++ williams-classic-arcade-game/js/app.js
@@ -3,8 +3,8 @@ var allEnemies = [];
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
+    this.x = x; //horizontal 
+    this.y = y; //vertical 
     this.speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -31,28 +31,34 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player{
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+       
+        this.update = function(dt) {
 
-var Player = function() {
-    this.update = function() {
+            //change in x
 
+             //change in y 
+
+        }
+        this.handleInput = function() {
+
+        }
+
+        this.render = function() {
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
+    
+        this.sprite = 'images/char-cat-girl.png';
     }
-
-    this.render = function() {
-        ctx.drawImage(Resources.get(this.sprite));
-    }
-
-    this.handleInput = function() {
-
-    }
-
-    this.sprite = 'images/char-cat-girl.png';
-};
-
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-allEnemies = [new Enemy()]
+//allEnemies = [new Enemy()]
 // function createEnemies(n) { //number of enemies created
-//     var enemies = {}
+//     var enemies = new Enemy()
 //     for (var i = 0; i < n; ++i) {
 //         enemies[i] = new Enemy()
 //     }
@@ -75,3 +81,18 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+
+
+// var Player = function() {
+//     this.update = function() {
+//     }
+//     this.render = function() {
+//         ctx.drawImage(Resources.get(this.sprite));
+//     }
+//     this.handleInput = function() {
+
+//     }
+//     this.sprite = 'images/char-cat-girl.png';
+// };
