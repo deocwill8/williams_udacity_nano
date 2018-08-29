937ac7eeef2c071fee7cc493fcd7ad46118f8713
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-08-28 21:44:16 -0500

    now rendering image and list following mvo pattern

--- cat_clicker_projects/cat_clicker_pre_redo/app.js
+++ cat_clicker_projects/cat_clicker_pre_redo/app.js
@@ -1,12 +1,10 @@
-// document.getElementById("header").innerText = header;
-// listOfLinks.addEventListener('click', getChildrenOfList);
-
 //imageClicked = function() {
 //         counter++
 //         //TODO figure out how to reference the name of the picture as well 
 //         counterText.innerText = `The picture, ${pictureTextHeader.innerText},  has been clicked ${counter} times.`;
 //         console.log(counter);
 // }
+let currentCat; 
 
 let model = {
 
@@ -20,52 +18,66 @@ let model = {
 }
 
 let octopus = {
-        getImageNames: function() {
-                console.log(model.listOfCats);
-                return model.listOfCats
+        getListOfNames: function() {
+                //console.log(model.listOfCats);
+                return model.listOfCats;
         },
+
         //increase cat count
         updateCount: function(catName) {
                 catName++
         },
-        //change the model/view
-        notifyView: function() {
 
-        }
+        //change the model/view
+        updateImgArea: function(nameOfImage) {
+                model.listOfCats.forEach(function(currentName){
+                        if(nameOfImage == currentName.catName) {
+                                currentCat = currentName;
+                                view.renderCurrentCat();
+                            }
+                })
+        },
+
+        // getCurrentCat(clickedCat){
+        //         console.log(clickedCat);
+        //         return clickedCat;
+        // }
+        
 }
 
 let view = {
-        init: function(){
-        //wrap in init function
-        this.header = `List of Cats`;
-        this.listOfLinks = document.getElementById("listOfLinks")
-        this.pictureContainer = document.getElementById('pictureContainer')
-        this.pictureTextHeader = document.getElementById('pictureTextHeader')
-        this.img = document.getElementById('picture')
-        this.counterText = document.getElementById('counterText')
-        },
-
         renderListOfNames: function(){
-                octopus.getImageNames().forEach(function(name){
-                        var li = document.createElement('li');
+                octopus.getListOfNames().forEach(function(name){
+                        let li = document.createElement('li');
                         listOfLinks.appendChild(li);
                         li.id = name.catName;
                         li.innerText += name.catName;
+
+
+                        li.addEventListener('click', function(catImageName){
+                                octopus.updateImgArea(catImageName.srcElement.textContent);
+                                console.log(catImageName.srcElement.textContent + " is the current cat")
+                        })
                 })
 
         },
 
-        //change current cat
-        updateImageArea: function(){
+        renderCurrentCat: function() {
+                currentImage = currentCat;
+                listOfLinks = document.getElementById("listOfLinks");
+                pictureContainer = document.getElementById('pictureContainer');
+                pictureTextHeader = document.getElementById('pictureTextHeader');
+                img = document.getElementById('picture');
+                counterText = document.getElementById('counterText');
+
+                pictureContainer.innerHTML = "",
+                pictureTextHeader.innerText = currentCat.catName;
+                img.src = currentCat.imgSrc;
+                pictureContainer.appendChild(pictureTextHeader);
+                pictureContainer.appendChild(img);
+                pictureContainer.appendChild(counterText);
+                counterText.innerText = `The picture, ${pictureTextHeader.innerText}, has been clicked`;  
 
         }
-
-        //pictureContainer.innerHTML = "",
-        // pictureTextHeader.innerText = "Cat 5";
-        // img.src = 'images/cat_5.jpg';
-        // pictureContainer.appendChild(pictureTextHeader);
-        // pictureContainer.appendChild(img);
-        // pictureContainer.appendChild(counterText);
-        // counterText.innerText = `The picture, ${pictureTextHeader.innerText}, has been clicked ${counter} times.`;
 }
 view.renderListOfNames();
