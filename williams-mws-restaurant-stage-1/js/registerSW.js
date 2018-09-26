97807e444a3c9f46721f86e89445ec081018900b
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-25 19:58:58 -0500

    adding service worker set up to the project

---
+++ williams-mws-restaurant-stage-1/js/registerSW.js
@@ -0,0 +1,16 @@
+//register new service worker for caching
+//files i need to cache
+//index.html
+//restaurant.html
+//main.js
+//restaurant_in.js
+//dbhelper.js
+//img folder
+
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('/index.js').then(function(reg){
+        console.log("Im registered");
+    }).catch(function(err){
+        console.log("not registered");
+    })
+}
---
+++ williams-mws-restaurant-stage-1/js/sw/index.js
@@ -0,0 +1,2 @@
+//listen for sw events
+console.log("hello");
