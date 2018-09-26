639eef46d63e921fbdc2ca918581f29e1dd2b1bb
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-26 18:30:24 -0500

    added caching to website

--- williams-mws-restaurant-stage-1/index.js
+++ williams-mws-restaurant-stage-1/index.js
@@ -1,36 +1,38 @@
-//files that need to be cached
-"https://maps.googleapis.com/maps-api-v3/api/js/34/10/common.js"
-"https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
-"/img/"
-"/js/main.js"
-"/js/dbhelper.js"
-"/js/restaurant_info.js"
-"/index.js"
-"/data/restaurants.json"
-"/css/styles.css"
-"/restaurant.html?id=1"
+const versionNumber = "williams-sw-v1"
 
-let versionNumber = "williams-sw-v1"
-
-self.addEventListener("fetch", function(event){
-    console.log("Ive been fetched");
-})
-
-self.addEventListener("installed", function(event){
+self.addEventListener("install", function(event){
     event.waitUntil(
         caches.open(versionNumber).then(function(cache){
             return cache.addAll([
                 "/",
-                "https://maps.googleapis.com/maps-api-v3/api/js/34/10/common.js",
-                "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700",
+                "/img",
                 "/js/main.js",
                 "/js/dbhelper.js",
                 "/js/restaurant_info.js",
                 "/index.js",
                 "/data/restaurants.json",
                 "/css/styles.css",
-                "/restaurant.html?id=1"
+                "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700",
+                'img/1.jpg',
+				'img/2.jpg',
+				'img/3.jpg',
+				'img/4.jpg',
+				'img/5.jpg',
+				'img/6.jpg',
+				'img/7.jpg',
+				'img/8.jpg',
+				'img/9.jpg',
+				'img/10.jpg'
             ])
         })
     )
 })
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response){
+          console.log(response)
+        return response || fetch(event.request)
+      })
+    );
+  });
--- williams-mws-restaurant-stage-1/js/registerSW.js
+++ williams-mws-restaurant-stage-1/js/registerSW.js
@@ -1,9 +1,8 @@
 if(navigator.serviceWorker){
     navigator.serviceWorker.register('index.js').then(function(reg){
-        console.log(reg);
-        console.log("Im registered");
+        //console.log("Im registered");
     }).catch(function(err){
-        console.log(err);
-        console.log("not registered");
+        //console.log(err);
+        //console.log("not registered");
     })
 }
