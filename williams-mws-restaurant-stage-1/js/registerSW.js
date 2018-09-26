2cd66093e69f50ec83f06e25ac249047c910ad5c
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-25 20:18:19 -0500

    moving sw to correct place in project

--- williams-mws-restaurant-stage-1/js/registerSW.js
+++ williams-mws-restaurant-stage-1/js/registerSW.js
@@ -8,9 +8,11 @@
 //img folder
 
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/index.js').then(function(reg){
+    navigator.serviceWorker.register('index.js').then(function(reg){
+        console.log(reg);
         console.log("Im registered");
     }).catch(function(err){
+        console.log(err);
         console.log("not registered");
     })
 }
