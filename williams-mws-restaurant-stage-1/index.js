306cc1e14c56bfaf925f764ed370d37cb0564ca5
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-26 07:45:55 -0500

    adding cache

--- williams-mws-restaurant-stage-1/index.js
+++ williams-mws-restaurant-stage-1/index.js
@@ -1,2 +1,36 @@
-//listen for sw events
-console.log("hello");
+//files that need to be cached
+"https://maps.googleapis.com/maps-api-v3/api/js/34/10/common.js"
+"https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
+"/img/"
+"/js/main.js"
+"/js/dbhelper.js"
+"/js/restaurant_info.js"
+"/index.js"
+"/data/restaurants.json"
+"/css/styles.css"
+"/restaurant.html?id=1"
+
+let versionNumber = "williams-sw-v1"
+
+self.addEventListener("fetch", function(event){
+    console.log("Ive been fetched");
+})
+
+self.addEventListener("installed", function(event){
+    event.waitUntil(
+        caches.open(versionNumber).then(function(cache){
+            return cache.addAll([
+                "/",
+                "https://maps.googleapis.com/maps-api-v3/api/js/34/10/common.js",
+                "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700",
+                "/js/main.js",
+                "/js/dbhelper.js",
+                "/js/restaurant_info.js",
+                "/index.js",
+                "/data/restaurants.json",
+                "/css/styles.css",
+                "/restaurant.html?id=1"
+            ])
+        })
+    )
+})
--- williams-mws-restaurant-stage-1/js/registerSW.js
+++ williams-mws-restaurant-stage-1/js/registerSW.js
@@ -1,12 +1,3 @@
-//register new service worker for caching
-//files i need to cache
-//index.html
-//restaurant.html
-//main.js
-//restaurant_in.js
-//dbhelper.js
-//img folder
-
 if(navigator.serviceWorker){
     navigator.serviceWorker.register('index.js').then(function(reg){
         console.log(reg);
