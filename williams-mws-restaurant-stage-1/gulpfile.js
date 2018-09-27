387c8b16d3920a37e57278d97d284817b7a32d2b
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-26 19:22:10 -0500

    added browser sync and gulpfile

--- williams-mws-restaurant-stage-1/gulpfile.js
+++ williams-mws-restaurant-stage-1/gulpfile.js
@@ -1,6 +1,17 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
+// function defaultTask(cb) {
+//     // place code for your default task here
+//     cb();
+//   }
   
-  exports.default = defaultTask
+//   exports.default = defaultTask
+
+// const gulp = require("gulp");
+// const browserSync = require("browser-sync").create();
+
+// gulp.task("default", ["styles"], function() {
+//   gulp.watch("css/**/*.css", ["styles"]);
+
+//   browserSync.init({
+//     server: "./"
+//   });
+// });
