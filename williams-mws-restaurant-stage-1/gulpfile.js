380e1a4f7984e526ac0d62ab8d4a2b4d7c13e090
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-27 07:43:22 -0500

    start to editing css commented sw stuff adding tasks to gulp file

--- williams-mws-restaurant-stage-1/gulpfile.js
+++ williams-mws-restaurant-stage-1/gulpfile.js
@@ -1,17 +1,55 @@
-// function defaultTask(cb) {
-//     // place code for your default task here
-//     cb();
-//   }
+// // function defaultTask(cb) {
+// //     // place code for your default task here
+// //     cb();
+// //   }
   
-//   exports.default = defaultTask
+// //   exports.default = defaultTask
 
-// const gulp = require("gulp");
-// const browserSync = require("browser-sync").create();
+// var gulp = require('gulp');
+// var browserify = require('gulp-browserify');
+// var concat = require('gulp-concat');
+// var less = require('gulp-less');
+// var refresh = require('gulp-livereload');
+// var lr = require('tiny-lr');
+// var server = lr();
+// var minifyCSS = require('gulp-minify-css');
+// var embedlr = require('gulp-embedlr');
+// var browserSync = require("browser-sync").create();
 
-// gulp.task("default", ["styles"], function() {
-//   gulp.watch("css/**/*.css", ["styles"]);
+// gulp.task('scripts', function() {
+//     gulp.src(['app/js/*.js'])
+//         .pipe(browserify())
+//         .pipe(concat('dest.js'))
+//         .pipe(gulp.dest('dist/build'))
+//         .pipe(refresh(server))
+// })
 
-//   browserSync.init({
-//     server: "./"
-//   });
-// });
+// gulp.task('styles', function() {
+//     gulp.src(['app/css/style.css'])
+//         .pipe(minifyCSS())
+//         .pipe(gulp.dest('dist/build'))
+//         .pipe(refresh(server))
+// })
+
+// gulp.task('html', function() {
+//     gulp.src("app/*.html")
+//         .pipe(embedlr())
+//         .pipe(gulp.dest('dist/'))
+//         .pipe(refresh(server));
+// })
+
+// gulp.task('default', function() {
+//     gulp.run('lr-server', 'scripts', 'styles', 'html');
+
+//     gulp.watch('app/src/**', function(event) {
+//         gulp.run('scripts');
+//     })
+
+//     gulp.watch('app/css/**', function(event) {
+//         gulp.run('styles');
+//     })
+
+//     gulp.watch('app/**/*.html', function(event) {
+//         gulp.run('html');
+//     })
+// })
--- williams-mws-restaurant-stage-1/index.js
+++ williams-mws-restaurant-stage-1/index.js
@@ -1,38 +1,38 @@
-const versionNumber = "williams-sw-v1"
+// const versionNumber = "williams-sw-v1"
 
-self.addEventListener("install", function(event){
-    event.waitUntil(
-        caches.open(versionNumber).then(function(cache){
-            return cache.addAll([
-                "/",
-                "/img",
-                "/js/main.js",
-                "/js/dbhelper.js",
-                "/js/restaurant_info.js",
-                "/index.js",
-                "/data/restaurants.json",
-                "/css/styles.css",
-                "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700",
-                'img/1.jpg',
-				'img/2.jpg',
-				'img/3.jpg',
-				'img/4.jpg',
-				'img/5.jpg',
-				'img/6.jpg',
-				'img/7.jpg',
-				'img/8.jpg',
-				'img/9.jpg',
-				'img/10.jpg'
-            ])
-        })
-    )
-})
+// self.addEventListener("install", function(event){
+//     event.waitUntil(
+//         caches.open(versionNumber).then(function(cache){
+//             return cache.addAll([
+//                 "/",
+//                 "/img",
+//                 "/js/main.js",
+//                 "/js/dbhelper.js",
+//                 "/js/restaurant_info.js",
+//                 "/index.js",
+//                 "/data/restaurants.json",
+//                 "/css/styles.css",
+//                 "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700",
+//                 'img/1.jpg',
+// 				'img/2.jpg',
+// 				'img/3.jpg',
+// 				'img/4.jpg',
+// 				'img/5.jpg',
+// 				'img/6.jpg',
+// 				'img/7.jpg',
+// 				'img/8.jpg',
+// 				'img/9.jpg',
+// 				'img/10.jpg'
+//             ])
+//         })
+//     )
+// })
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response){
-          console.log(response)
-        return response || fetch(event.request)
-      })
-    );
-  });
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       caches.match(event.request).then(function(response){
+//           console.log(response)
+//         return response || fetch(event.request)
+//       })
+//     );
+//   });
