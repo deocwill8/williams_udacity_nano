d3858145451753ab5737c0e14bddfa1a1ea77a13
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-30 08:18:33 -0500

    adding sw back

--- williams-mws-restaurant-stage-1/js/registerSW.js
+++ williams-mws-restaurant-stage-1/js/registerSW.js
@@ -1,8 +1,8 @@
-// if(navigator.serviceWorker){
-//     navigator.serviceWorker.register('index.js').then(function(reg){
-//         //console.log("Im registered");
-//     }).catch(function(err){
-//         //console.log(err);
-//         //console.log("not registered");
-//     })
-// }
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('index.js').then(function(reg){
+        //console.log("Im registered");
+    }).catch(function(err){
+        //console.log(err);
+        //console.log("not registered");
+    })
+}
