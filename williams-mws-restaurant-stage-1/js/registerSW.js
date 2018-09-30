60b6bcf11b3cd2f9ea0121101b9ee5cbc8c8f07d
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-09-30 08:37:52 -0500

    readme changes

--- williams-mws-restaurant-stage-1/js/registerSW.js
+++ williams-mws-restaurant-stage-1/js/registerSW.js
@@ -2,7 +2,7 @@ if(navigator.serviceWorker){
     navigator.serviceWorker.register('index.js').then(function(reg){
         //console.log("Im registered");
     }).catch(function(err){
-        //console.log(err);
-        //console.log("not registered");
+        console.log(err);
+        console.log("not registered");
     })
 }
