bd1ef8db18546ee0ead14fb6b0b074e32719c048
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-10-06 09:52:31 -0500

    added routing to application

--- williams-react-myreads/src/App.js
+++ williams-react-myreads/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import CurrentlyReading from './CurrentlyReading'
 import WantToRead from './WantToRead'
 import Read from './Read'
+import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
     currentlyReading: [],
     wantToReady: [],
-    read: [],
-    showSearchPage: false
+    read: []
   }
 
   componentDidMount(){
@@ -22,22 +23,28 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <CurrentlyReading />
-                <WantToRead />
-                <Read />
-              </div>
-            </div> 
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+      <Route exact path="/" render={() => (
+                  <div className="list-books">
+                  <div className="list-books-title">
+                    <h1>MyReads</h1>
+                  </div>
+                  <div className="list-books-content">
+                    <div>
+                      <CurrentlyReading />
+                      <WantToRead />
+                      <Read />
+                    </div>
+                  </div> 
+                  <div className="open-search">
+                  <Link
+                    to="/search"
+                    >Add a book
+                  </Link>
+                  </div>
+                </div>
+      )}>
+      </Route>
+      <Route path="/search" component={Search}></Route>
       </div>
     )
   }
