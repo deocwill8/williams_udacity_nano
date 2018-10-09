54c8e4fae78bac5f313a4158fdc3a754b2d0ae7e
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-10-08 20:49:35 -0500

    broke book out of search book component

---
+++ williams-react-myreads/src/Components/FilteredBookComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class FilteredBookComponent extends Component {
+    componentDidMount(){
+        console.log('Book' ,this.props)
+    } 
+
+    render() {
+      return (
+        <div className="book">
+        <div className="book-top">
+            {/* ideal for templating the image url came from Udacity mentor Ryan Waite's YouTube walk through: https://www.youtube.com/watch?v=acJHkd6K5kI&t=4s */}
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
+            <div className="book-shelf-changer">
+            <select value={this.props.book.shelf || "none"}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+             </div>
+         </div>
+        <div className="book-title">{this.props.book.title}</div>
+        {/* make this more readable */}
+        {/* <div className="book-authors">{this.props.book.authors.map(author => author)}</div> */}
+        </div>
+      )
+    }
+}
+
+export default FilteredBookComponent
--- williams-react-myreads/src/Components/Search.js
+++ williams-react-myreads/src/Components/Search.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 import * as BooksAPI from '../BooksAPI'
+import FilteredBookComponent from './FilteredBookComponent';
 
 
 
@@ -11,7 +12,8 @@ class Search extends Component {
     super(props)
     this.state = {
       searchQuery: '',
-      bookMatches: []
+      bookMatches: [],
+      isError: false
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -35,14 +37,6 @@ class Search extends Component {
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input type="text" 
                 placeholder="Search by title or author"
                 value={this.state.searchQuery}
@@ -54,25 +48,8 @@ class Search extends Component {
               <ol className="books-grid">
                   {this.state.bookMatches.map((searchedBook) => (
                     <li key={searchedBook.id}>
-                      <div className="book">
-                        <div className="book-top">
-                            {/* ideal for templating the image url came from Udacity mentor Ryan Waite's YouTube walk through: https://www.youtube.com/watch?v=acJHkd6K5kI&t=4s */}
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${searchedBook.imageLinks.thumbnail})`}}></div>
-                            <div className="book-shelf-changer">
-                            <select value={searchedBook.shelf || "none"}>
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                             </div>
-                         </div>
-                        <div className="book-title">{searchedBook.title}</div>
-                        {/* make this more readable */}
-                        {/* <div className="book-authors">{searchedBook.authors.map(author => author)}</div> */}
-                        </div>
-                      </li>
+                      <FilteredBookComponent book={searchedBook} />
+                    </li>
                   ))}
               </ol>
             </div>
