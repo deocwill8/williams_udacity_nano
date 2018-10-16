f84ba0fa8ab58ba4bf54906233608050ef814e11
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-10-15 20:00:30 -0500

    adding geocoding to the project

--- google-maps-exercise/app.js
+++ google-maps-exercise/app.js
@@ -143,6 +143,10 @@ function initMap(){
             toggleDrawing(drawingManager);
         });
 
+        document.getElementById('zoom-to-area').addEventListener('click', function() {
+        zoomToArea();
+      });
+
         drawingManager.addListener('overlaycomplete', function(event){
             if(polygon){
                 polygon.setMap(null);
@@ -255,4 +259,33 @@ for (var i = 0; i < markers.length; i++) {
     }
     }
 }
+
+      // This function takes the input value in the find nearby area text input
+      // locates it, and then zooms into that area. This is so that the user can
+      // show all listings, then decide to focus on one area of the map.
+      function zoomToArea() {
+        // Initialize the geocoder.
+        var geocoder = new google.maps.Geocoder();
+        // Get the address or place that the user entered.
+        var address = document.getElementById('zoom-to-area-text').value;
+        // Make sure the address isn't blank.
+        if (address == '') {
+          window.alert('You must enter an area, or address.');
+        } else {
+          // Geocode the address/area entered to get the center. Then, center the map
+          // on it and zoom in
+          geocoder.geocode(
+            { address: address,
+              componentRestrictions: {locality: 'New York'}
+            }, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                map.setZoom(15);
+              } else {
+                window.alert('We could not find that location - try entering a more' +
+                    ' specific place.');
+              }
+            });
+        }
+      }
 
